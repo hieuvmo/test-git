a394cc553ef5911d3b2f0c465f8f032ac73bac17
Viết Minh Hiếu <hieuvm1@vmodev.com>
2022-09-05 17:03:33 +0700

    test: commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Commit1 from "./Commit1";
+import Commit2 from "./Commit2";
+import Commit3 from "./Commit3";
 
 function App() {
   return (
     <>
       <Commit1 />
+      <Commit2 />
+      <Commit3 />
     </>
   );
 }
---
+++ src/Commit3.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Commit3 = () => {
+  return <div>Commit 3</div>;
+};
+
+export default Commit3;
